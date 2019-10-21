aaa0e071d19e61f10c3ca5bef4c74f8ca0f961ad
jdacevedo10 <53051401+jdacevedo10@users.noreply.github.com>
2019-10-21 16:14:22 -0500

    Update BinaryTree.java

--- laboratorios/lab04/codigo/BinaryTree.java
+++ laboratorios/lab04/codigo/BinaryTree.java
@@ -1,9 +1,11 @@
-public class BinaryTree {
+public class BinaryTree 
+{
 	public Node root;
  
-	public BinaryTree(){
+	public BinaryTree()
+	{
   root = null;
- }
+	}
 	private int max2(int i, int j)
 	{
 		if (i > j)
@@ -14,16 +16,41 @@ public class BinaryTree {
 	private int maxheightAUX(Node node)
 	{
 		if (node == null)
-				return 0;
+			return 0;
 		else 
-				return max2(maxheightAUX(node.left), maxheightAUX(node.right))+1;
+			return max2(maxheightAUX(node.left), maxheightAUX(node.right))+1;
 	}
 	
 	public int maxheight()
 	{
 	   return maxheightAUX(root);
 	}
-
+	
+	
+    private boolean inser(Node nodo, String nom, String g){
+        if(nodo.data == nom){
+            return false;
+        }
+        if(g.equalsIgnoreCase("mujer")){
+            if(nodo.left == null){
+                nodo.left = new Node(nom);
+                return true;
+            }
+            else{
+                return inser(nodo.left, nom, g);
+            }
+        }
+        if(g.equalsIgnoreCase("hombre")){
+            if(nodo.right == null){
+                nodo.right = new Node(nom);
+                return true;
+            }
+            else{
+                return inser(nodo.right, nom, g);
+            }
+        }
+        return false;
+    }
 	private void recursivePrintAUX(Node node)
 	{
 		if (node != null)
